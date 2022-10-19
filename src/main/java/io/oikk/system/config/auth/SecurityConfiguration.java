952b09e40d88d80b4c54e33098655168a3b76c6a
chanjin_ahn <olkkani@gmail.com>
2022-10-19 15:36:50 +0900

    Style: update main controller

--- src/main/java/io/oikk/system/config/auth/SecurityConfiguration.java
+++ src/main/java/io/oikk/system/config/auth/SecurityConfiguration.java
@@ -1,12 +1,6 @@
 package io.oikk.system.config.auth;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfiguration {
