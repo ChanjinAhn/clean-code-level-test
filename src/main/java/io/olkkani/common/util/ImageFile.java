d36f7626222b80e2fd7a51e0caee0022ab70255b
chanjin_ahn <olkkani@gmail.com>
2022-11-03 18:04:50 +0900

    :sparkles: add text convert hash
    이미지 저장시 중복 방지를 위한 파일명 해시화 기능

---
+++ src/main/java/io/olkkani/common/util/ImageFile.java
@@ -0,0 +1,48 @@
+package io.olkkani.common.util;
+
+import java.io.File;
+import java.math.BigInteger;
+import java.nio.file.Paths;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Base64;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class ImageFile {
+
+    String filePath = Paths.get("question_images").toString();
+
+    public String fileNameToHash (String fileName) {
+        try {
+            // 난수 생성
+            SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
+            byte[] bytes = new byte[16];
+            random.nextBytes(bytes);
+            // salt 생성
+            String salt = new String(Base64.getEncoder().encode(bytes));
+            fileName = fileName + salt;
+            // sha-256 + salt hash
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            md.update(fileName.getBytes());
+            return String.format("%064x", new BigInteger(1, md.digest()));
+        } catch (NoSuchAlgorithmException e) {
+            log.error("not found algorithm, please check algorithm get instance", e);
+        }
+        return fileName;
+    }
+
+    public File getFileByFileName (String fileName) {
+        return Paths.get(filePath,fileName).toFile();
+    }
+
+    public void save (String fileName, File imageFile) {
+//        try {
+//            BufferedImage image =
+//        }catch (IOException e){
+//            log.error("",e);
+//        }
+    }
+
+}
