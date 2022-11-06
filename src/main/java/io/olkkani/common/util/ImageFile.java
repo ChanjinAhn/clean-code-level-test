0c6bdaff60e49cec44f953c50c0f3f5341bcc478
chanjin_ahn <olkkani@gmail.com>
2022-11-06 21:45:15 +0900

    :sparkles: Add image file util
    이미지 저장과 불러오기를 위한 util class

--- src/main/java/io/olkkani/common/util/ImageFile.java
+++ src/main/java/io/olkkani/common/util/ImageFile.java
@@ -8,6 +8,9 @@ import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.util.Base64;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
+import org.springframework.web.multipart.MultipartFile;
 
 @Slf4j
 public class ImageFile {
@@ -37,12 +40,30 @@ public class ImageFile {
         return Paths.get(filePath,fileName).toFile();
     }
 
-    public void save (String fileName, File imageFile) {
+    public void saveImageFile (String fileName, File imageFile) {
 //        try {
 //            BufferedImage image =
 //        }catch (IOException e){
 //            log.error("",e);
 //        }
     }
+
+
+    public String saveFileAndReturnHashedFileName (MultipartFile imageFile) {
+        String originalFileName = imageFile.getOriginalFilename();
+        String hashedImageFIleName = fileNameToHash(FilenameUtils.getBaseName(originalFileName));
+        String extension = FilenameUtils.getExtension(originalFileName);
+
+//        imageFile.
+//        saveImageFile(im);
+
+        return hashedImageFIleName + "." + extension;
+    }
+
+    public MultipartFile getMultipartFileByFileName (String fileName){
+
+        return null;
+    }
+
 
 }
