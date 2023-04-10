d4bd077b5fae26787033fcfaa8eadcc05c954f00
chanjin_ahn <olkkani@gmail.com>
2023-04-10 18:06:45 +0900

    :heavy_plus_sign: Add Dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,11 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "3.0.5"
 	id("io.spring.dependency-management") version "1.1.0"
+
 	kotlin("jvm") version "1.7.22"
 	kotlin("plugin.spring") version "1.7.22"
+	kotlin("plugin.jpa") version  "1.7.22"
+	kotlin("kapt") version "1.7.22"
 }
 
 group = "io.olkkani"
@@ -14,11 +17,39 @@ java.sourceCompatibility = JavaVersion.VERSION_17
 repositories {
 	mavenCentral()
 }
-
+configurations {
+	all {
+		// was tomcat 제외
+		exclude(module = "spring-boot-starter-tomcat")
+	}
+}
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter")
+	// kotlin
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	// common
+	implementation("org.apache.commons:commons-text:1.10.0")
+	implementation("org.apache.commons:commons-lang3:3.12.0")
+	implementation("commons-io:commons-io:2.11.0")
+	implementation("io.github.microutils:kotlin-logging-jvm:3.0.5")
+	// spring
+	implementation("org.springframework.boot:spring-boot-starter")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+	// was
+	implementation("org.springframework.boot:spring-boot-starter-undertow")
+	// database, jdbc
+
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	// view template
+	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+	implementation("nz.net.ultraq.thymeleaf:thymeleaf-layout-dialect")
+	// test
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("com.ninja-squad:springmockk:4.0.0")
+
+	kapt("org.springframework.boot:spring-boot-configuration-processor")
+
 }
 
 tasks.withType<KotlinCompile> {
