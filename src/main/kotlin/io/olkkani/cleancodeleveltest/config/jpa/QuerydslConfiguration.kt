663e47dae611ecfcddbde07542951d57fa619b14
chanjin_ahn <olkkani@gmail.com>
2023-04-19 20:29:03 +0900

    :wrench: QueryDsl 설정 추가

---
+++ src/main/kotlin/io/olkkani/cleancodeleveltest/config/jpa/QuerydslConfiguration.kt
@@ -0,0 +1,17 @@
+package io.olkkani.cleancodeleveltest.config.jpa
+
+import com.querydsl.jpa.impl.JPAQueryFactory
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import javax.persistence.EntityManager
+import javax.persistence.PersistenceContext
+
+@Configuration
+class QuerydslConfiguration {
+
+    @PersistenceContext
+    lateinit var entityManager: EntityManager
+
+    @Bean
+    fun jpaQueryFactory(): JPAQueryFactory = JPAQueryFactory(entityManager)
+}
