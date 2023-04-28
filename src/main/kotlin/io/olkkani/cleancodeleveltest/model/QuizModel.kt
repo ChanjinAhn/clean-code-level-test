455f68a5ea70f3c5b5f919373c5cb7c4e6141462
chanjin_ahn <olkkani@gmail.com>
2023-04-28 18:28:52 +0900

    :bulb: 버그 제거 Todo 코멘트 삭제 (버그 해결)

--- src/main/kotlin/io/olkkani/cleancodeleveltest/model/QuizModel.kt
+++ src/main/kotlin/io/olkkani/cleancodeleveltest/model/QuizModel.kt
@@ -2,7 +2,6 @@ package io.olkkani.cleancodeleveltest.model
 
 import io.olkkani.cleancodeleveltest.domain.Quiz
 
-// request model 을 제대로 불러오지 못하는 이유 찾기
 data class QuizRequest(
     val question: String,
     val optionA: String,
