11e4d0eecae6e91f5bc270dea74e0393cc07c83c
chanjin_ahn <olkkani@gmail.com>
2023-05-09 22:34:52 +0900

    :lipstick: Thymeleaf quiz_management_layout 추가

---
+++ src/main/resources/static/js/quiz.js
@@ -0,0 +1,17 @@
+function moveRegisterPage(){
+    window.location.href = "/register"
+}
+
+function moveEditPage(id){
+    window.location.href = "editor/" + id
+}
+
+
+function validateQuizForm() {
+
+}
+
+
+function moveListPage() {
+    location.replace("list")
+}
