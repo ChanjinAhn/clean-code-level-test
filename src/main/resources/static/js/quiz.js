e09a1ebe93d46cb0276f5ea359aeb5e4cd83cdbd
chanjin_ahn <olkkani@gmail.com>
2023-05-31 23:52:40 +0900

    :sparkles: serialize 추가

--- src/main/resources/static/js/quiz.js
+++ src/main/resources/static/js/quiz.js
@@ -15,3 +15,23 @@ function validateQuizForm() {
 function moveListPage() {
     location.replace("/list")
 }
+
+
+function serialize (rawData) {
+    let rtnData = {};
+    for (let [key, value] of rawData) {
+        let sel = document.querySelectorAll("[name=" + key + "]");
+        // Array Values
+        if (sel.length > 1) {
+            if (rtnData[key] === undefined) {
+                rtnData[key] = [];
+            }
+            rtnData[key].push(value);
+        }
+        // Other
+        else {
+            rtnData[key] = value;
+        }
+    }
+    return rtnData;
+}
