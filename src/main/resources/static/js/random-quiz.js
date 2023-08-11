a7d60514343a00944c853ecd7290ed2dda2c3285
chanjin_ahn <olkkani@gmail.com>
2023-08-11 15:17:39 +0900

    :lipstick: update html file code case
    camel case 로 변경

---
+++ src/main/resources/static/js/random-quiz.js
@@ -0,0 +1,8 @@
+
+const questionTypeText = document.getElementById("questionTypeText")
+const questionTypeCode = document.getElementById("questionTypeCode")
+function clearQuestionBlock(){
+    questionTypeText.innerHTML = ""
+    questionTypeCode.innerHTML = ""
+}
+
