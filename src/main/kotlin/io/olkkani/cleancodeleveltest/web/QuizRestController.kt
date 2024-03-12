0e0541c196440b1e84fd6e343ed58a3f847f4138
chanjin_ahn <olkkani@gmail.com>
2024-03-12 16:33:46 +0900

    :art: add ResponseEntity

--- src/main/kotlin/io/olkkani/cleancodeleveltest/web/QuizRestController.kt
+++ src/main/kotlin/io/olkkani/cleancodeleveltest/web/QuizRestController.kt
@@ -1,9 +1,11 @@
 package io.olkkani.cleancodeleveltest.web
 
 import QuizRequest
+import QuizResponse
 import io.olkkani.cleancodeleveltest.service.QuizService
 import org.springframework.data.domain.PageRequest
 import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import toPaginationResponse
 import toResponse
@@ -18,38 +20,45 @@ class QuizRestController(
     @ResponseStatus(HttpStatus.CREATED)
     fun create(
         @RequestBody request: QuizRequest
-    ) {
+    ): ResponseEntity.HeadersBuilder<*> {
         quizService.create(request)
+        return ResponseEntity.noContent()
     }
 
     @GetMapping
     fun getAll(
         @RequestParam(defaultValue = "10") perPage : Int,
         @RequestParam(defaultValue = "0") page : Int
-    ) = quizService.getAll(PageRequest.of(page-1,perPage)).toPaginationResponse()
+    )= ResponseEntity.ok().body(quizService.getAll(PageRequest.of(page-1,perPage)).toPaginationResponse())
+
 
     @GetMapping("/{id}")
     fun get(
         @PathVariable id: Long
-    ) = quizService.get(id).toResponse()
+    ): ResponseEntity<QuizResponse>
+        = ResponseEntity.ok(quizService.get(id).toResponse())
 
     @GetMapping("/random")
-    fun getRandomQuizzes() = quizService.getRandomList()?.map { it.toResponse() }
+    fun getRandomQuizzes(): ResponseEntity<List<QuizResponse>>
+        = ResponseEntity.ok().body(quizService.getRandomList()?.map { it.toResponse() })
+
 
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     fun edit (
         @PathVariable id: Long,
         @RequestBody request: QuizRequest
-    ) {
+    ): ResponseEntity.BodyBuilder {
         quizService.edit(id, request)
+        return ResponseEntity.accepted()
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     fun delete(
         @PathVariable id: Long
-    ) {
+    ): ResponseEntity.HeadersBuilder<*> {
         quizService.delete(id)
+        return ResponseEntity.noContent()
     }
 }
