11133cb4e6c307e0ba716cd6ba6d4e7d3620be2e
chanjin_ahn <olkkani@gmail.com>
2024-03-26 20:06:26 +0900

    :heavy_plus_sign: add validation

--- build.gradle.kts
+++ build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
 	// was
 	implementation("org.springframework.boot:spring-boot-starter-undertow")
 	// database, jdbc
