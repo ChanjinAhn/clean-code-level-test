e65e4c8808e73db144901e1e3a43474f5fbb39ff
chanjin_ahn <olkkani@gmail.com>
2024-03-26 20:06:58 +0900

    :heavy_plus_sign: add tailwindcss animate

--- src/main/resources/static/tailwind.config.js
+++ src/main/resources/static/tailwind.config.js
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-      "../**/*.{html,js}"
-  ],
-  theme: {
-    extend: {
+    content: [
+        "../**/*.{html,js}"
+    ],
+    theme: {
+        extend: {},
     },
-  },
-  plugins: [],
+    plugins: [
+        require("tailwindcss-animate"),
+    ],
 }
 
